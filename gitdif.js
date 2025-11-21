c1dbb6285d05a4b5d82d2d793a5ee7bcfa1f1a54
Deepu23e1 <dpal34036@gmail.com>
2025-11-21 20:18:20 +0530

    shere2ndcommitfh

--- gitdif.js
+++ gitdif.js
@@ -4,6 +4,12 @@ Git Differences Analyzer
 A tool to analyze and display differences between Git commits or branches.
 </p>
 </h1>
+<h2>Usage</h2>
+<p>
+To use this tool, ensure you have Node.js and the 'simple-git' package installed. You can install 'simple-git' using npm:   </p>
+<h1>
+    again ready for more changes
+</h1>
 </html>
 
 const simpleGit = require('simple-git');
