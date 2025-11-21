e0385aa560181735f5b133e5d0798c5d9cf9c8aa
Deepu23e1 <dpal34036@gmail.com>
2025-11-21 19:54:14 +0530

    shere we see git diffh

---
+++ gitdif.js
@@ -0,0 +1,10 @@
+<html><h1>
+Git Differences Analyzer
+<p>
+A tool to analyze and display differences between Git commits or branches.
+</p>
+</h1>
+</html>
+
+const simpleGit = require('simple-git');
+const git = simpleGit();    
